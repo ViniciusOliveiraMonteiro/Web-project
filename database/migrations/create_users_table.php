ea75ae6fd75a7f0d9d28299c7e6befbc31fa5ab6
Vinicius Monteiro <viniciussppt@gmail.com>
2019-11-10 22:22:18 -0300

    Creating models, migrations and controllers

---
+++ database/migrations/create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('idUser');
+            $table->string('name');
+            $table->string('lastname');
+            $table->integer('phone');
+            $table->string('password');            
+            $table->integer('user_type')->default(1);
+            $table->string('email')->unique();            
+            $table->rememberToken();            
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
